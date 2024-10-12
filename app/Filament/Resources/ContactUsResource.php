b16882ed35b95842b0647dafc56235b1b03cf62f
bsmala ibrahem <bosiebrahem487@gmail.com>
2024-10-12 23:08:50 +0200

    add contact us to user navigationGroup

--- app/Filament/Resources/ContactUsResource.php
+++ app/Filament/Resources/ContactUsResource.php
@@ -17,6 +17,8 @@ class ContactUsResource extends Resource
 {
     protected static ?string $model = ContactUs::class;
 
+    protected static ?string $navigationGroup = 'User';
+
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function getPluralModelLabel(): string
