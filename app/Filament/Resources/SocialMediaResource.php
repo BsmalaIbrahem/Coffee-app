d2542e404b65d1502a1384c2efc0a2ca26a1364d
bsmala ibrahem <bosiebrahem487@gmail.com>
2024-10-12 17:04:00 +0200

    social media resource

---
+++ app/Filament/Resources/SocialMediaResource.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SocialMediaResource\Pages;
+use App\Filament\Resources\SocialMediaResource\RelationManagers;
+use App\Models\SocialMedia;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class SocialMediaResource extends Resource
+{
+    protected static ?string $model = SocialMedia::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('type')
+                ->label(__('filament.Type'))
+                ->options([
+                    'Phone' => 'Phone',
+                    'Whatsapp' => 'Whatsapp',
+                    'Gmail' => 'Gmail',
+                    'Location' => 'Location',
+                    'Facebook' => "Facebook",
+                    "Instagram" => "Instagram",
+                ])
+                ->searchable()
+                ->required(),
+
+                Forms\Components\TextInput::make('value')
+                ->label(__('filament.Value'))
+                ->required()
+                ->maxLength(255)
+                ->unique(),
+
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('type')
+                ->label(__('filament.Type'))
+                ->searchable(),
+
+                Tables\Columns\TextColumn::make('value')
+                ->label(__('filament.Value'))
+                ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSocialMedia::route('/'),
+            'create' => Pages\CreateSocialMedia::route('/create'),
+            'edit' => Pages\EditSocialMedia::route('/{record}/edit'),
+        ];
+    }
+}
