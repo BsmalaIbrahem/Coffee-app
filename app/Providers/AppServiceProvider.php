0b70cf83235ee7681c9afc9eb22b576f9ba7d5d2
bsmala ibrahem <bosiebrahem487@gmail.com>
2024-10-17 16:18:12 +0200

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,9 +29,7 @@ class AppServiceProvider extends ServiceProvider
         });
 
         View::composer('*', function ($view) {
-            $categories = Category::with(['products' => function($q){
-                $q->take(3);
-            }])->take(3)->get();
+            $categories = Category::with(['products'])->take(3)->get();
             $view->with('categories', $categories);
         });
     }
