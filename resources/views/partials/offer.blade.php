3391702ad80444d3abedd03e251331894e97bc2a
bsmala ibrahem <bosiebrahem487@gmail.com>
2024-10-21 06:56:58 +0200

    view offer

--- resources/views/partials/offer.blade.php
+++ resources/views/partials/offer.blade.php
@@ -1,7 +1,7 @@
 <div  style="background-color:#E59A59;">
   <a href="products.html" style="text-decoration:none;">
     <p style="padding:12px 0;" class="text-center h4 text-white">
-        Discount up to 60 %
+        Discount up to {{$offer['value']}} {{$offer['type_flag']}}
     </p>
   </a>
 </div>
