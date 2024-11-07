519acc4acf152a9a5f7d40521962db1498d74035
bsmala ibrahem <bosiebrahem487@gmail.com>
2024-11-07 22:03:38 +0200

    return discounted products

--- app/Http/Controllers/Web/DiscountController.php
+++ app/Http/Controllers/Web/DiscountController.php
@@ -5,20 +5,29 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\ProductService;
+use App\Services\DiscountService;
 
 class DiscountController extends Controller
 {
-    private $service;
+    private $productService; private $service;
 
-    public function __construct(ProductService $service)
+    public function __construct(DiscountService $service, ProductService $productService)
     {
         $this->service = $service;
+        $this->productService = $productService;
     }
 
     public function index()
     {
-        $products = $this->service->get(function($q){
-            $q->whereHas('offers');
+        $last_offer = $this->service->get(function($q){
+            $q->where('is_active', true)->latest('id');
+        }, false, [], false, true);
+
+
+        $products = $this->productService->get(function($q) use ($last_offer){
+            $q->whereHas('offers', function($q) use ($last_offer){
+                $q->where('offer_id', $last_offer['id']);
+            });
         }, true,  [], false);
 
         return view('product', ['products' => $products]);
